b75465967cf26348c772c0d6ad8897cc06d6e2f3
tanhoangviet <128269328+tanhoangviet@users.noreply.github.com>
2024-04-25 07:49:43 +0700

    Update toggle.lua

--- toggle.lua
+++ toggle.lua
@@ -8,13 +8,11 @@ ScreenGui.ResetOnSpawn = false
 local Toggle = Instance.new("TextButton")
 Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
-Toggle.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Toggle.BackgroundTransparency = 1
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
 Toggle.Size = UDim2.new(0, 90, 0, 38)
 Toggle.Font = Enum.Font.SourceSans
-Toggle.Text = "Tutorial"
-Toggle.TextColor3 = Color3.fromRGB(248, 248, 248)
-Toggle.TextSize = 28.000
+Toggle.Text = ""
 Toggle.Draggable = true
 
 -- Add a UICorner to the Toggle button
@@ -22,19 +20,16 @@ local Corner = Instance.new("UICorner")
 Corner.Name = "Corner"
 Corner.Parent = Toggle
 
--- Function to handle LeftControl key press event
-local function HandleLeftControlKeyPress(input)
-    if input.KeyCode == Enum.KeyCode.LeftControl then
-        -- Toggle the visibility of the GUI
-        ScreenGui.Enabled = not ScreenGui.Enabled
-    end
-end
-
--- Connect LeftControl key press event
-game:GetService("UserInputService").InputBegan:Connect(HandleLeftControlKeyPress)
-
 -- Function to handle the Toggle button click event
 Toggle.MouseButton1Click:Connect(function()
     -- Toggle the visibility of the GUI
     ScreenGui.Enabled = not ScreenGui.Enabled
+end)
+
+-- Connect the LeftControl key press event
+game:GetService("UserInputService").InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.LeftControl then
+        -- Toggle the visibility of the GUI
+        ScreenGui.Enabled = not ScreenGui.Enabled
+    end
 end)
